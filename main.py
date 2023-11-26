eeda692f329707d47f694df23397776a98be2df2
uv0037 <yuvrajajitgavhane.com>
2023-11-26 19:39:33 +0000

    Model evaluation completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
 STAGE_NAME ="Data Ingestion stage"
@@ -29,8 +30,17 @@ except Exception as e:
 STAGE_NAME="TRAINING"
 try:
     logger.info(f">>> stage {STAGE_NAME} started <<<")
-    obj = ModelTrainingPipeline()
-    obj.main()
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<")
+except Exception as e:
+    raise e
+
+STAGE_NAME="Evaluation stage"
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    model_evalution = EvaluationPipeline()
+    model_evalution.main()
     logger.info(f">>> stage {STAGE_NAME} completed <<<")
 except Exception as e:
     raise e
---
+++ src/cnnClassifier/pipeline/stage_04_evaluation.py
@@ -0,0 +1,25 @@
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.evaluation import Evaluation
+from cnnClassifier import logger
+
+
+STAGE_NAME = "EVALUATION STAGE"
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        val_config = config.get_validation_config()
+        evaluation = Evaluation(val_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>> stage {STAGE_NAME} started <<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>> stage {STAGE_NAME} completed <<<")
+    except Exception as e:
+        raise e
